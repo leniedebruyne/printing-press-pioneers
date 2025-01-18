67f977172ab27f1e23aa7f40cf47ff476d7849c1
Lenie Debruyne <lenie.debruyne@student.howest.be>
2025-01-18 10:45:29 +0100

    interactie roepen voor plantin te laten ontsnappen

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ const menu = document.querySelector('.nav__menu');
 const sections = document.querySelectorAll('section');
 const links = document.querySelectorAll('.nav__list a');
 
+const image = document.querySelector('.section4__plantin');
+let position = 0;
+
 
 hamburger.addEventListener('click', () => {
   menu.classList.toggle('visible');
@@ -32,10 +35,43 @@ const setActiveLink = () => {
 };
 
 
+const setupVoiceDetection = () => {
+  navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+    const audioContext = new AudioContext();
+    const source = audioContext.createMediaStreamSource(stream);
+
+    const analyser = audioContext.createAnalyser();
+    analyser.fftSize = 256;
+    source.connect(analyser);
+
+    // maakt array die groot genoeg is voor de gemeten getallen op te slaan, deze word dan gevuld met data van analysernode
+    const dataArray = new Uint8Array(analyser.frequencyBinCount);
+
+    const detectVolume = () => {
+      analyser.getByteFrequencyData(dataArray);
+      // tel alle waarden met elkaar op en bereken het gemiddelde
+      const volume = dataArray.reduce((a, b) => a + b, 0) / dataArray.length;
+
+      if (volume > 75) {
+        position += 10;
+        image.style.transform = `translateX(${position}px)`;
+      }
+
+      requestAnimationFrame(detectVolume);
+    };
+
+    detectVolume();
+  })
+};
+
 
 
 const init = () => {
   setActiveLink();
+  setupVoiceDetection();
 }
 
 init();
+
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Visualizations_with_Web_Audio_API
